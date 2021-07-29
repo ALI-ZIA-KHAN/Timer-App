ff0bbd52944e3203c78825bdd3300453c7034aa4
ALI-ZIA -KHAN <azk443.443@gmail.com>
2021-07-29 23:16:19 +0500

    my try

---
+++ public/app.js
@@ -0,0 +1,90 @@
+var millisec = 1000
+var sec ;
+var min ;
+var displaymili = document.getElementById('displaymiliSec')
+var displaysec = document.getElementById('displaySec')
+var displaymin = document.getElementById('displayMin')
+
+
+
+
+
+function userinput(){
+
+    var user_input_min=+prompt("Enter min","eg 1-60")
+    var user_input_sec=+prompt("Enter sec","eg 1-60")
+    // console.log(user_input)
+    // console.log(displaymin)
+
+    displaymin.innerHTML=user_input_min;
+    displaysec.innerHTML=user_input_sec;
+     min=user_input_min;
+     sec=user_input_sec;
+     alert("Your Time Starts Now")
+    
+
+  
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+function timer() {
+    millisec--
+    displaymili.innerHTML = millisec
+    if (millisec == 0) {
+        sec--
+        displaysec.innerHTML = sec
+        millisec = 1000
+        if (sec == 0) {
+            min--
+            sec = 60
+            displaymin.innerHTML = min;
+            if(min==0 && sec==0){
+                clearInterval(interval)
+                alert("Time ends")
+            }
+        }
+    }
+   
+}
+
+var interval;
+
+function start() {
+    interval = setInterval(timer, 1);
+
+}
+function pause() {
+    clearInterval(interval)
+}
+function reset() {
+    pause()
+    millisec = 0
+    sec = 0
+    min = 0
+    displaymili.innerHTML = millisec
+    displaysec.innerHTML = sec
+    displaymin.innerHTML = min
+}
+
+
+
+// function foo(){
+//     console.log(1)
+// }
+
+// setTimeout(foo,1000)
+// setInterval(foo,1000)
